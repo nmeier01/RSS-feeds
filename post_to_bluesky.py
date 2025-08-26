30ea2a1c88b94824b94a4ec7c2786821a008e892
nmeier01 <124737572+nmeier01@users.noreply.github.com>
2025-08-26 12:12:30 -0700

    Update post_to_bluesky.py

--- post_to_bluesky.py
+++ post_to_bluesky.py
@@ -72,11 +72,11 @@ elif "description" in latest and "<img" in latest.description:
 
 # Only post if it's new
 if link != last_posted_link:
-    post_text = f"Update from Tumblr: {title}\n{link}"
+    post_text = f"Update from Tumblr: {title}"
     
     max_len = 300
-    if len(title)>max_len:
-        post_text = title[:max_len-3]+"…"
+    if len(post_text)>max_len:
+        post_text = post_text[:max_len-1]+"…"
         
     image_urls = extract_images(latest)
 
@@ -136,6 +136,7 @@ else:
 
 
  
+
 
 
 
